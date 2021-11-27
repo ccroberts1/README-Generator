8f18ec8c872b016c93344752b3498784ae30649a
Caitlin Roberts <caitlin.coy.roberts@gmail.com>
2021-11-26 18:00:55 -0700

    added inquirer prompt and .then to create basic readme file, still needs refinement

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 // TODO: Create an array of questions for user input
-const questions = inquirer.prompt([
+const questions = [
   {
     type: "input",
     message: "What is the title of your project?",
@@ -10,17 +10,17 @@ const questions = inquirer.prompt([
   },
   {
     type: "input",
-    message: "Briefly describe your project:",
+    message: "Please provide a brief description of the project:",
     name: "description",
   },
   {
     type: "input",
-    message: "Please provide installation instructions:",
+    message: "Please provide step-by-step instructions for installation:",
     name: "install",
   },
   {
     type: "input",
-    message: "What usage information would you like to provide?",
+    message: "Please provide instructions for usage:",
     name: "usage",
   },
   {
@@ -58,15 +58,49 @@ const questions = inquirer.prompt([
     message: "What is your email?",
     name: "email",
   },
-]);
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-  fs.writeFile(`${fileName}`, data);
+function writeToFile(fileName, data, callback) {
+  fs.writeFile(`${fileName}.md`, data, callback);
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions).then((response) => {
+    const result = `# ${response.title}
+## Description
+${response.description}
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+- [Badges](#badges)
+- [How to Contribute](#contributing)
+- [Tests](#tests)
 
-// Function call to initialize app
+## Installation
+${response.install}
+## Usage
+${response.usage}
+## Credits
+
+## License
+${response.license}
+## Badges
+
+## Contributing
+${response.contribution}
+## Tests
+${response.test}`;
+    writeToFile("examplereadme", result, (err) => {
+      err
+        ? console.log("Oops, something went wrong! Please try again")
+        : console.log("Success!");
+    });
+  });
+}
+
+// TODO: Function call to initialize app
 init();
